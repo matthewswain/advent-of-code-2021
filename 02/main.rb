edfe37f05b888f7eca77240249c63a395e66b242
Matthew Swain <matthew.swain@receipt-bank.com>
2021-12-02 08:16:38 +0000

    Solve day two part two

--- 02/main.rb
+++ 02/main.rb
@@ -15,3 +15,17 @@ input.each do |direction, value|
 end
 
 puts "Part 1: #{x * y}"
+
+x, y, aim = 0, 0, 0
+input.each do |direction, value|
+  case direction
+  when 'up' then aim -= value
+  when 'down' then aim += value
+  when 'forward' then y += value; x += (aim * value)
+  when 'back' then y -= value; x -= (aim * value)
+  else raise "Invalid direction: #{direction}"
+  end
+
+end
+
+puts "Part 2: #{x * y}"
